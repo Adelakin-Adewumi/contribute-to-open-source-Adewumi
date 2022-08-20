6bf08018723edac2088b6e537206584d1be7e117
Adewumi <adelakinadewumi@gmail.com>
2022-08-20 01:35:10 -0700

    Update calculator.js
    I made a little changes on the modulus. I hope it suits what you want.

--- src/calculator.js
+++ src/calculator.js
@@ -45,4 +45,13 @@ exports.divide = (x, y) => {
   return x / y;
 };
 
-module.exports = exports;
+//module.exports = exports;
+exports.module = (x, y) => {
+  if (typeof x !== 'number') {
+    throw new TypeError(`${x} is not a number`);
+  }
+  if (typeof y !== 'number') {
+    throw new TypeError(`${y} is not a number`);
+  }
+  return x % y;
+};
